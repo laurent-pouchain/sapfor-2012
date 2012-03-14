9ab9ad1289a5acfe9a58dfd015e8933a223f7576
Antoine Rocheteau <26003872@e008m08.istic.univ-rennes1.fr>
2012-03-14 17:29:20 +0100

    MAJ Client

--- sapfor.client/src/istic/sapfor/client/command/ICommandContextKey.java
+++ sapfor.client/src/istic/sapfor/client/command/ICommandContextKey.java
@@ -2,10 +2,8 @@ package istic.sapfor.client.command;
 
 public enum ICommandContextKey {
 
-<<<<<<< HEAD
-	Key_Stage, Key_login;
-=======
-	Key_Stage, Key_Insct;
->>>>>>> 0ab4bff2d578b4c0703d37c138e114c5dec7170f
+
+	Key_Stage, Key_login, Key_Insct;
+
 	
 }
